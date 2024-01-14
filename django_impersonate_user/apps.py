82fe727af2d8d3e9d71da99afac78f087fc7ace4
fikra <fikra@practical-devsecops.com>
2024-01-14 18:08:01 +0700

    fix: :bug: use User content Type

--- django_impersonate_user/apps.py
+++ django_impersonate_user/apps.py
@@ -7,10 +7,13 @@ class ImpersonateUserConfig(AppConfig):
     label = "impersonate"
 
     def ready(self) -> None:
+        from django.contrib.auth import get_user_model
         from django.contrib.auth.models import Permission
+        from django.contrib.contenttypes.models import ContentType
 
+        content_type = ContentType.objects.get_for_model(get_user_model())
         Permission.objects.get_or_create(
             name="impersonate_user",
-            content_type__name="global_permission",
+            content_type=content_type,
             codename="impersonate",
         )
