45e848946dd8b1f9d921ae95f72badb796ec9a79
fikra <fikra@practical-devsecops.com>
2024-01-14 18:24:20 +0700

    fix: use proxy model

--- django_impersonate_user/apps.py
+++ django_impersonate_user/apps.py
@@ -7,11 +7,11 @@ class ImpersonateUserConfig(AppConfig):
     label = "impersonate"
 
     def ready(self) -> None:
-        from django.contrib.auth import get_user_model
+        from django_impersonate_user.models import DjangoImpersonateUser
         from django.contrib.auth.models import Permission
         from django.contrib.contenttypes.models import ContentType
 
-        content_type = ContentType.objects.get_for_model(get_user_model())
+        content_type = ContentType.objects.get_for_model(DjangoImpersonateUser)
         Permission.objects.get_or_create(
             name="impersonate_user",
             content_type=content_type,
--- django_impersonate_user/models.py
+++ django_impersonate_user/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 
 # Create your models here.
+class DjangoImpersonateUser(models.Model):
+    class Meta:
+        proxy = True
