d9022cd0093e1beb0ed2f97527b48bbd557f057d
fikra <fikra@practical-devsecops.com>
2024-01-14 16:39:45 +0700

    feat: :truck: move permissions to apps

--- django_impersonate_user/apps.py
+++ django_impersonate_user/apps.py
@@ -1,7 +1,16 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Permission
 
 
 class ImpersonateUserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_impersonate_user"
     label = "impersonate"
+
+    def ready(self) -> None:
+        Permission.objects.get_or_create(
+            name="impersonate_user",
+            content_type__name="global_permission",
+            app_label=self.label,
+            code_name="impersonate",
+        )
--- django_impersonate_user/models.py
+++ django_impersonate_user/models.py
@@ -2,12 +2,3 @@ from django.db import models
 
 
 # Create your models here.
-class ImpersonateSupport(models.Model):
-    class Meta:
-        default_permissions = ()  # disable defaults add, delete, view, change perms
-        permissions = (
-            (
-                "impersonate_user",
-                "impersonate or login as other user for admin diagnostic purposes",
-            ),
-        )
