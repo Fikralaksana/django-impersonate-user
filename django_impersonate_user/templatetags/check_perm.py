fef78f6c217a96a08d983b380c97e20d6b56103a
fikra <fikra@practical-devsecops.com>
2024-01-15 12:04:52 +0700

    fix: :bug: use app label

--- django_impersonate_user/templatetags/check_perm.py
+++ django_impersonate_user/templatetags/check_perm.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 def has_impersonate_perm(user: AbstractUser):
     """Removes all values of arg from the given string"""
-    return user.has_perm(f"{user._meta.label}.impersonate")
+    return user.has_perm(f"{user._meta.app_label}.impersonate")
 
 
 register.filter("has_impersonate_perm", has_impersonate_perm)
